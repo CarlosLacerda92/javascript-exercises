de779f93e6a443704eaec1cfeab5688c3513529c
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-04-06 07:53:44 -0300

    feat: complete exercise 03 - Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const arrayOfCharacters = [...string];
+    let reversedString      = '';
+
+    for (let i = (arrayOfCharacters.length - 1); i >= 0; i--) {
+        reversedString += arrayOfCharacters[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
