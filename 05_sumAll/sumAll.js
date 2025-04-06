fbbc44e9e5e0c601b4981e39deff8284cf5dd463
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-04-06 16:46:54 -0300

    feat: complete exercise 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    if (Math.floor(num1) < num1 || Math.floor(num2) < num2) {
+        return 'ERROR';
+    }
+
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+
+    if (num2 > num1) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = num1; i >= num2; i--) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
