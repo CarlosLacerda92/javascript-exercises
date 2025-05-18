e8bf3a754a6785d12b69086dd1a158f6109d3edf
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-05-18 14:54:36 -0300

    feat: complete exercise 09 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function(str) {
 
+    let arr    = [];
+    let newStr = '';
+
+    str = str.toLowerCase();
+    str = str.replace(/[^a-zA-Z0-9]/g, '');
+    
+    arr = str.split('');
+    arr.reverse();
+
+    //newStr = arr.join('');
+
+    newStr = arr.reduce((text, current) => {
+        return text + current;
+    }, '');
+
+    if (newStr != str) {
+        return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
