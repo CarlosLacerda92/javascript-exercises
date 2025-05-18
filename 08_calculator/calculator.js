083ae7b69aa5dc8a9dc9750fb031c798ce2e7666
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-05-18 09:07:39 -0300

    feat: complete exercise 08 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return Number(a) + Number(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return Number(a) - Number(b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
+const power = function(of, to) {
 	
+  let result = of;
+
+  for (let i = 1; i < to; i++) {
+    result *= of;
+  }
+
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(number) {
 	
+  let result = 1;
+
+  if (number === 0) {
+    return result;
+  }
+
+  result = number;
+
+  for (let i = (number - 1); i >= 1; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
