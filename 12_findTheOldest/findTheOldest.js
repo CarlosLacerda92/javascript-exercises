a2cf4e7b6b421c3d51323133921015edb892a6ee
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-05-18 15:42:06 -0300

    feat: complete exercise 12 - findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    return people.reduce((accumulator, current) => {
+
+        if (!accumulator.yearOfDeath) {
+            accumulator.yearOfDeath = new Date().getFullYear();
+        }
+
+        if (!current.yearOfDeath) {
+            current.yearOfDeath = new Date().getFullYear();
+        }
+
+        let accAge  = accumulator.yearOfDeath - accumulator.yearOfBirth;
+        let currAge = current.yearOfDeath - current.yearOfBirth;
+
+        return accAge > currAge ? accumulator : current;
+    });
 };
 
 // Do not edit below this line
