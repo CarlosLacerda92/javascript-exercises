dd1404a58502a0b0d57b7f9c723e6c87243ad4b0
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-05-18 15:20:05 -0300

    feat: complete exercise 11 - getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((acc, current) => {
+        acc.push(current.title);
+        return acc;
+    }, []);
 };
 
 // Do not edit below this line
