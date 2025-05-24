3243cd99edcfe055771daf02776e8ba5b8d82707
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-05-24 10:18:44 -0300

    feat: complete exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    number = Number(number);
+
+    if (number < 0) {
+        return 'OOPS';
+    }
+
+    if (number === 0) {
+        return 0;
+    }
+
+    const sequence = [1];
+
+    for (let i = 0; i < number; i++) {
+
+        const previousNumber = (i - 1) < 0 ? 0 : sequence[i - 1];
+        const currentNumber  = sequence[i];
+        
+        let fibbonaciNumber = previousNumber + currentNumber;
+
+        sequence.push(fibbonaciNumber);
+    }
+
+    return sequence[number - 1];
 };
 
 // Do not edit below this line
